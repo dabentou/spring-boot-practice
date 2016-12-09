538730b6bad3e419dea90d0fb90f0b1ff21c5cbd
Administrator <513213884@qq.com>
2016-12-09 17:04:17 +0800

    Initial commit

---
+++ src/main/java/tian/Application.java
@@ -0,0 +1,23 @@
+package tian;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Administrator on 2016/12/9.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String greeting(){
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("tds");
+        SpringApplication.run(Application.class,args);
+    }
+}
